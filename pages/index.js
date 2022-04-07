23d772e1a6446e6ed16e7e35916078f4008b1aee
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-04-07 12:33:10 +0800

    Update some stuff

--- pages/index.js
+++ pages/index.js
@@ -8,25 +8,28 @@ const Index = () => {
   return (
     <div>
       <h1>Dark Mode</h1>
-      {theme !== undefined && (
-        <select value={theme} onChange={e => setTheme(e.target.value)}>
-          <option value="dark">Dark</option>
-          <option value="light">Light</option>
-          <option value="system">System</option>
-        </select>
-      )}
+      <p>A simple dark mode test using Next.js</p>
+    <main>{/* prettier-ignore */}
+        {theme !== undefined && (
+          <select value={theme} onChange={e => setTheme(e.target.value)} aria-label="Select color theme">
+            <option value="dark">Dark</option>
+            <option value="light">Light</option>
+            <option value="system">System</option>
+          </select>
+        )}
 
-      <br />
-      <br />
+        <br />
+        <br />
 
-      <div id="options">
-        <Link href="/dark">
-          <a>Forced Dark Page</a>
-        </Link>
-        <Link href="/light">
-          <a>Forced Light Page</a>
-        </Link>
-      </div>
+        <div id="options">
+          <Link href="/dark">
+            <a>Forced Dark Page</a>
+          </Link>
+          <Link href="/light">
+            <a>Forced Light Page</a>
+          </Link>
+        </div>
+      </main>
     </div>
   )
 }
